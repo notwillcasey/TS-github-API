e2fa3728faaba2a7f1fdce2e19989c4d9409bcf8
William Casey <notwillcasey@gmail.com>
2022-01-15 22:41:37 -0700

    error handling and testing init

--- src/controllers/repo-controllers.ts
+++ src/controllers/repo-controllers.ts
@@ -5,36 +5,39 @@ interface Repo {
   name: string,
   description: string,
   url: string
-}
+};
 
-  const getRepos = async function (req: Request, res: Response) {
+const getRepos = async function (req: Request, res: Response) {
 
-    const user: any = req.query.user;
-    const url: string = `https://api.github.com/users/${user}/repos`;
-    const options: object = {
-      headers: {
-        'User-Agent': 'request',
-        'Authorization': `token ${process.env.TOKEN}`
-      }
-    };
-
-    try {
-      const result: AxiosResponse = await axios.get(url, options);
-      const repositories: [Repo] = result.data.map((rep: any) => {
-        const repository: Repo = {
-          name: rep.name,
-          description: rep.description,
-          url: rep.url
-        };
-        return repository;
-      })
-      return res.status(200).send(repositories);
-
-    } catch (e) {
-      console.log(e);
-      return res.status(400).send(e);
+  const user: any = req.query.user;
+  const url: string = `https://api.github.com/users/${user}/repos`;
+  const options: object = {
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': `token ${process.env.TOKEN}`
     }
+  };
 
+  try {
+    const result: AxiosResponse = await axios.get(url, options);
+    const repositories: [Repo] = result.data.map((rep: any) => {
+      const repository: Repo = {
+        name: rep.name,
+        description: rep.description,
+        url: rep.url
+      };
+      return repository;
+    })
+    return res.status(200).send(repositories);
+
+  } catch (e: any) {
+    if (e.response.statusText === 'Not Found') {
+      return res.status(404).send('username not found')
+    }
+    console.log(e);
+    return res.status(400).send(e);
   }
 
-export default { getRepos }
+};
+
+export default { getRepos };
